999cda53a9565c0421bb5ea895ae3111775470fe
Bill Dueber <bill@dueber.com>
2017-07-24 12:08:05 -0400

    Remove broken sort

--- lib/inspector.rb
+++ lib/inspector.rb
@@ -12,11 +12,7 @@ class Inspector
     head + sum(rest)
   end
 
-  def max(lst)
-    _max(lst, 0)
-  end
-
-  def _max(lst, current_highest)
+  def max(lst, current_highest = 0)
     return current_highest if lst.empty?
     head, rest = fshift(lst)
     _max(rest, head > current_highest ?  head : current_highest)
@@ -33,29 +29,7 @@ class Inspector
     head,rest = fshift(lst)
     reverse(rest).push(head)
   end
-
-  def sort(lst)
-    return [] if lst.empty?
-    return lst if lst.size == 1
-
-    if lst.size == 2
-      return merge([lst.first], [lst.last])
-    end
-
-    midpoint = lst.size / 2
-    first_half = lst[0..midpoint]
-    last_half  = lst[midpoint + 1..-1]
-    merge(sort(first_half), sort(last_half))
-  end
-
-  def merge(l1, l2)
-    return [l1, l2].flatten if l1.empty? or l2.empty?
-    if l1.size == 1 and l2.size == 1
-      l1.first > l2.first ? [l2, l1].flatten : [l1, l2].flatten
-    else
-      merge(sort(l1), sort(l2))
-    end
-  end
+  
 
 
 end
