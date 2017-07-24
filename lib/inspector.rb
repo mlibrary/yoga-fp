19e3f28105d2e738e4f2fe5262bf4953ce8230f5
Greg Kostin <gkostin@umich.edu>
2017-07-24 11:57:20 -0400

    checkpoint

---
+++ lib/inspector.rb
@@ -0,0 +1,30 @@
+class Inspector
+  def sum(input)
+    return 0 if input.empty?
+    input[0] + sum(input[1..-1])
+  end
+
+  def max(input)
+    return 0 if input.empty?
+    head = input[0]
+    rest = max(input[1..-1])
+    (head > rest) ? head : rest
+  end
+
+  def square(input)
+    return input if input.empty?
+    square(input[1..-1]).unshift input[0]*input[0]
+  end
+
+  def reverse(input)
+    return input if input.empty?
+    reverse(input[1..-1]) << input[0]
+  end
+
+  def sort(input)
+    return input if input.empty?
+    return input if input.size == 1
+    rest = sort(input[1..-1])
+    (input[0] < max(rest)) ? rest.unshift(input[0]) : rest << input[0]
+  end
+end
