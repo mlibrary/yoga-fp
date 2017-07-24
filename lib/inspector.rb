bd39b99746dad107e18dbab5a0d92ecdff29df4d
Greg Kostin <gkostin@umich.edu>
2017-07-24 15:38:40 -0400

    bubble sort

--- lib/inspector.rb
+++ lib/inspector.rb
@@ -6,27 +6,22 @@ class Inspector
 
   def max(input)
     return 0 if input.empty?
-    head = input[0]
-    rest = max(input[1..-1])
-    (head > rest) ? head : rest
+    (input[0] > max(input[1..-1])) ? input[0] : max(input[1..-1])
   end
 
   def square(input)
     return input if input.empty?
-    square(input[1..-1]).unshift input[0]*input[0]
+    [input[0]*input[0]] + square(input[1..-1])
   end
 
   def reverse(input)
     return input if input.empty?
-    reverse(input[1..-1]) << input[0]
+    reverse(input[1..-1]) + [input[0]]
   end
 
   def sort(input)
-    return input if input.empty?
-    return input if input.size == 1
+    return input if input.size < 2
     return (input[0] < input[1]) ? input : reverse(input) if input.size == 2
-    head = input[0]
-    rest = input[1..-1]
-    (head >= rest[0])  ? rest.unshift(input[0]) : rest << input[0]
+    sort(input[0...input.index(max(input))] + input[input.index(max(input))+1..-1]) + [max(input)]
   end
 end
